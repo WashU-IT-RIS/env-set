c87941547465f5d0b42fe227af4ab8451ee72337
Anthony Brummett <abrummett@wustl.edu>
2024-12-03 16:00:54 -0600

    Parse the args given and build up an Environment
    Looks for args of the form
        varname=$othervar
    and
        varname=value
    and builds an environment that contains the changes required
    
    ITDEV-36038

--- envset.go
+++ envset.go
@@ -14,16 +14,21 @@ func main() {
     // The first argv index that's the executable we were asked to run
     // in the container.  Subsequent args are params to this executable.
     first_exec_idx := -1
+
+    env := os.Environ()
+
     for i := 1; i < len(os.Args); i++ {
         copy_match := env_copy_regex.FindStringSubmatch(os.Args[i])
         if len(copy_match) > 0 {
             fmt.Printf("idx %d check copy regex returned %d: %s\n", i, len(copy_match), strings.Join(copy_match, ","))
+            env = append(env, fmt.Sprintf("%s=%s", copy_match[1], os.Getenv(copy_match[2])))
             continue
         }
 
         set_match := env_set_regex.FindStringSubmatch(os.Args[i])
         if len(set_match) > 0 {
             fmt.Printf("idx %d check set regex returned %d: %s\n", i, len(set_match), strings.Join(set_match, ","))
+            env = append(env, fmt.Sprintf("%s=%s", set_match[1], set_match[2]))
             continue
         }
 
